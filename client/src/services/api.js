b1a03ea6e26f5b4126d8ffe181c36118e756fe5c
SpiX <spicesauctionofficial10@gmail.com>
2026-03-10 12:20:53 +0530

    fixed api route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spix-auction-server.onrender.com"
+  baseURL: "https://spix-auction-server.onrender.com/api"
 });
 
 export default API;
